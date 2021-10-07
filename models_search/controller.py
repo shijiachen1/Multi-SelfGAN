634b532b4b95bd05363dff456bfc670bf8ca6f7f
shijiachen1 <40195415+shijiachen1@users.noreply.github.com>
2021-10-07 19:06:40 +0800

    Update controller.py

--- models_search/controller.py
+++ models_search/controller.py
@@ -1,10 +1,4 @@
 
-# -*- coding: utf-8 -*-
-# @Date    : 2019-09-29
-# @Author  : Xinyu Gong (xy_gong@tamu.edu)
-# @Link    : None
-# @Version : 0.0
-
 import numpy as np
 import torch
 import torch.nn as nn
