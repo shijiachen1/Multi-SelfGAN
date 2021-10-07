844c010f7c7f5e9e4e05635c1456e5a49f3b1016
shijiachen1 <40195415+shijiachen1@users.noreply.github.com>
2021-10-07 18:59:51 +0800

    Update search_block.py

--- search_block.py
+++ search_block.py
@@ -1,10 +1,4 @@
 
-# -*- coding: utf-8 -*-
-# @Date    : 2019-09-25
-# @Author  : Xinyu Gong (xy_gong@tamu.edu)
-# @Link    : None
-# @Version : 0.0
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
